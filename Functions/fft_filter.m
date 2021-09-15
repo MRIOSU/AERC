9f04b915bff5cede17e5b42e55c09ffaf3b80a82
Chong Chen <30392188+MRIOSU@users.noreply.github.com>
2021-09-15 12:22:05 -0400

    Update fft_filter.m

--- Functions/fft_filter.m
+++ Functions/fft_filter.m
@@ -1,9 +1,9 @@
-function filtered_x = fft_filter(x,Ts,f_low,f_high)
+function x = fft_filter(x,Ts,f_low,f_high)
 %Input:      x             [N, C]  (N:signal length, C:number of channel)
 %            Ts            temporal resolution in second
 %            f_low,f_high  low/high frequency cut
-%Output:     filtered_x    [N, C]
-%Last modified on 09/24/2020 by Chong Chen (chong.chen@osumc.edu)
+%Output:     x             [N, C] (filtered image)
+%Last modified on 09/15/2021 by Chong Chen (chong.chen@osumc.edu)
 
 %length of the signal
 N=size(x,1);                               
@@ -13,13 +13,13 @@ else
   f=(-(N-1)/2:(N-1)/2)/(Ts*N);
 end
 %DFT
-Xs=fftshift(fft(x),1);                     
+x=fftshift(fft(x),1);                     
 % rectangle window in the frequency domain
 lowf_idx = find(abs(f) < f_low);
 highf_idx = find(abs(f) > f_high);
-Xs([lowf_idx,highf_idx],:) = 0;
+x([lowf_idx,highf_idx],:) = 0;
 % inverse DFT
-filtered_x = ifft(ifftshift(Xs,1));
+x = ifft(ifftshift(x,1));
 
 
 
